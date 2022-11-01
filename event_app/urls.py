cc2b95a909ae6658d79e329b6e0ff1687be6da4a
Jishna mk <jishnamk9@gmail.com>
2022-11-01 14:36:26 +0530

    added a event

--- event_app/urls.py
+++ event_app/urls.py
@@ -7,6 +7,7 @@ urlpatterns = [
     path("signup",views.signup,name="signup"),
     path("signin",views.signin,name="signin"),
     path("signout",views.signout,name="signout"),
+    path("cancel/<int:aid>",views.cancel,name="cancel"),
     
     
     
